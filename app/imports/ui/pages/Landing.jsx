cdb7826be66090384317a7f055da76e75c02d614
Nathan Zenger <43054888+nzenger@users.noreply.github.com>
2019-11-02 11:17:51 -1000

    landing page mockup finished in 10 minutes

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Grid } from 'semantic-ui-react';
+import Icon from 'semantic-ui-react/dist/commonjs/elements/Icon';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div className={'background'}>
+        <Grid verticalAlign='top' textAlign='center' container>
 
           <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
+            <Icon name='inverted users' size={'huge'}/>
+            <h1>Multiple Users</h1>
+            <p>This address book enables any number of users to register and save their business contacts.
+              You can only see the contacts you have created.</p>
           </Grid.Column>
 
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
+          <Grid.Column width={4}>
+            <Icon name='inverted file alternate' size={'huge'}/>
+            <h1>Contact Details</h1>
+            <p>For each contact, you can save their name, address, and phone number. </p>
           </Grid.Column>
 
+          <Grid.Column width={4}>
+            <Icon name='inverted calendar check outline' size={'huge'}/>
+            <h1>Timestamped Notes</h1>
+            <p>Each time you make contact with a contact, you can write a note that summarizes the conversation.
+              This note is saved along with a timestamp with the contact.</p>
+          </Grid.Column>
         </Grid>
+        </div>
     );
   }
 }
